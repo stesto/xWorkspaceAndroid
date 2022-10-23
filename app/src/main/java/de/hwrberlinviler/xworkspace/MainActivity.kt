50dc773eeb479a1ac55c691b706007668e497cbb
stesto <s.stodder@hotmail.de>
2022-10-23 21:56:26 +0200

    Login und Cards hinzugefügt

--- app/src/main/java/de/hwrberlinviler/xworkspace/MainActivity.kt
+++ app/src/main/java/de/hwrberlinviler/xworkspace/MainActivity.kt
@@ -1,11 +1,19 @@
 package de.hwrberlinviler.xworkspace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import de.hwrberlinviler.xworkspace.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtnLoginClick(view: View) {
+        var loginAct = Intent(this, LoginActivity::class.java);
+        startActivity(loginAct);
     }
 }
