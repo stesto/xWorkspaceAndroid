565dfbc50ffb1c7f080777f1fc85006148ff1506
stesto <s.stodder@hotmail.de>
2022-11-16 10:15:56 +0100

    Login Button disablen beim Login

--- app/src/main/java/de/hwrberlinviler/xworkspace/LoginActivity.kt
+++ app/src/main/java/de/hwrberlinviler/xworkspace/LoginActivity.kt
@@ -3,6 +3,7 @@ package de.hwrberlinviler.xworkspace
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -21,6 +22,9 @@ class LoginActivity : AppCompatActivity() {
     fun Login(view: View) {
         val txtUsername = findViewById<EditText>(R.id.txtLoginUsername)
         val txtPassword = findViewById<EditText>(R.id.txtLoginPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin);
+
+        btnLogin.isEnabled = false
 
         lifecycleScope.launch {
             val loginRespose = XWorkspaceAPI.client.loginUser(
@@ -43,6 +47,10 @@ class LoginActivity : AppCompatActivity() {
                 .setTitle(R.string.login_failed_title)
                 .setMessage(R.string.login_failed_text)
                 .show()
+
+            val btnLogin = findViewById<Button>(R.id.btnLogin);
+
+            btnLogin.isEnabled = true
         }
     }
 }
